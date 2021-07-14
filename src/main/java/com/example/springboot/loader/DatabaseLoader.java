95eabeafca14db2ea30e260859d7337db2ca93f5
novakvova <novakvova@gmail.com>
2021-07-14 20:05:58 +0300

    Add seeder database

---
+++ src/main/java/com/example/springboot/loader/DatabaseLoader.java
@@ -0,0 +1,28 @@
+package com.example.springboot.loader;
+
+import com.example.springboot.entities.Category;
+import com.example.springboot.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public DatabaseLoader(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(this.categoryRepository.count()==0)
+        {
+            this.categoryRepository.save(new Category("Ковбаса"));
+            this.categoryRepository.save(new Category("Сало"));
+        }
+    }
+}
---
+++ src/main/java/com/example/springboot/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.springboot.repositories;
+
+import com.example.springboot.entities.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
