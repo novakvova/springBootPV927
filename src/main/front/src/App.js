9b0135742156ee64f0601c50c5cc61f848742672
novakvova <novakvova@gmail.com>
2021-09-06 18:24:27 +0300

    Add fornt

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -2,13 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import CategoryList from './components/CategoryCard/CategoryList';
 import CreateCategory from './components/CategoryCard/CreateCategory';
+import HomePage from './components/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import LoginPage from './components/auth/Login';
+import RegisterPage from './components/auth/Register';
+import NavbarHome from './components/NavbarHome';
 
 function App() {
   return (
-    <div>
-      <CategoryList></CategoryList>
-      <CreateCategory></CreateCategory>
-    </div>
+
+    <Router>
+      <NavbarHome />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/register">
+            <RegisterPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
